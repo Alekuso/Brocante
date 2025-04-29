09d8340d87001294dd5b4d2d985e9c21216a2448
Alex <alexandre.olemans@gmail.com>
2025-04-29 19:38:35 +0200

    fixes footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,10 @@
 <footer>
-    <p>Supra Brocante - 2024 ~ 2025</p>
+    <p class="footer-nom">Supra Brocante - 2024 ~ 2025</p>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="brocanteurs.php">Brocanteurs</a></li>
+        <li><a href="objets.php">Objets</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+    <p class="footer-addresse">Du 10 au 12 mars - Rue Grand Pré, Flémalle 4400</p>
 </footer>
-
-<?php ?>
